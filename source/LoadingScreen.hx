1b08129f0c415dfe10a584536aba8c8632ded476
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-09 19:16:04 +0800

    Create LoadingScreen.hx

---
+++ source/LoadingScreen.hx
@@ -0,0 +1,27 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.FlxG;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
+import flixel.system.FlxBasePreloader;
+
+using StringTools;
+
+class LoadingScreen extends FlxBasePreloader
+{
+	public function new(MinDisplayTime:Float = 0) 
+	{
+		super(2);
+	}
+
+	override public function create() 
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float) 
+	{
+		super.update(elapsed);
+	}
+}
