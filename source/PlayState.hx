2813588d9cd0bad6960f9a43e178580dbf9d4083
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-09 18:48:31 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,16 +7,28 @@ import flixel.util.FlxColor;
 import flixel.text.FlxText;
 import flixel.tweens.FlxTween;
 import flixel.tweens.FlxEase;
+import flixel.system.FlxSound;
+
+using StringTools;
 
 class PlayState extends FlxState
 {
+	var moan:FlxSound;
+	public static var ending:String;
+
 	override public function create()
 	{
+		moan = FlxG.sound.load(AssetPaths.animoo_moan__ogg);
+
 		super.create();
 	}
 
 	override public function update(elapsed:Float)
 	{
+		if (FlxG.keys.justPressed.M) {
+			moan.play();
+		}
+
 		super.update(elapsed);
 	}
 }
