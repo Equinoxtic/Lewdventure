3e774d6d5440a58bc592e2d90fa87ae23d897766
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-10 02:44:09 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,24 +1,15 @@
 package;
 
-import flixel.FlxState;
 import flixel.FlxGame;
 import openfl.display.Sprite;
-import openfl.display.FPS;
 
 class Main extends Sprite
 {
-	var gameWidth:Int = 1280;
-	var gameHeight:Int = 720;
-	var initialState:Class<FlxState> = LoadingState;
-	var zoom:Float = -1;
-	var framerate:Int = 60;
-	var skipSplash:Bool = true;
 	var startFullscreen:Bool = false;
-	public static var fpsVar:FPS;
 
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		addChild(new FlxGame(1280, 720, LoadingState, 1, 60, 60, true));
 	}
 }
