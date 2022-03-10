a6300b1188460c8a72af71299476ac16491024ff
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-10 20:07:11 +0800

    Create OptionsMenuState.hx

---
+++ source/OptionsMenuState.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.FlxState;
+
+using StringTools;
+
+class OptionsMenuState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
