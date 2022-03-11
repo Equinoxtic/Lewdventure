77d1326e242338dce1703c20ae99db4dd8f143fa
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-11 14:56:15 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,24 +4,39 @@ import flixel.FlxG;
 import flixel.FlxState;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
 import flixel.text.FlxText;
 import flixel.tweens.FlxTween;
 import flixel.tweens.FlxEase;
 import flixel.system.FlxSound;
+import flixel.addons.effects.FlxTrail;
 
 using StringTools;
 
 class PlayState extends FlxState
 {
 	var moan:FlxSound;
+	var player:FlxSprite;
+	var died:Bool = false;
+	var currentLevel:String = "";
+	var npc:FlxSprite;
+	var npcversion:String = "";
 	public static var ending:String;
 
 	override public function create()
 	{
 		moan = FlxG.sound.load(AssetPaths.animoo_moan__ogg);
 
+		player = new FlxSprite().makeGraphic(15, 15, FlxColor.BLUE);
+		player.x = FlxG.width / 2 - player.width / 2;
+		player.y = FlxG.height / 2 - player.height / 2;
+		add(player);
+
 		super.create();
 	}
+
+	var sped:Int = 0;
+	var zoomin:Bool = false;
 
 	override public function update(elapsed:Float)
 	{
@@ -29,6 +44,30 @@ class PlayState extends FlxState
 			moan.play();
 		}
 
+		if (FlxG.keys.justPressed.SPACE) {
+			zoomin = true;
+			new FlxTimer().start(20, function(tmr:FlxTimer) {
+				zoomin = true;
+			});
+		}
+
+		updateMovement();
+
 		super.update(elapsed);
+	}
+
+	function updateMovement()
+	{
+		if (zoomin) { sped = 10; }
+		else { sped = 5; }
+
+		var _up:Bool = FlxG.keys.anyPressed([UP, W]);
+		var _down:Bool = FlxG.keys.anyPressed([DOWN, S]);
+		var _left:Bool = FlxG.keys.anyPressed([LEFT, A]);
+		var _right:Bool = FlxG.keys.anyPressed([RIGHT, D]);
+		if (_up) player.y -= sped;
+		if (_down) player.y += sped;
+		if (_left) player.x -= sped;
+		if (_right) player.x += sped;
 	}
 }
