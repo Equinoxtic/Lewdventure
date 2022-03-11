be4aff31d6440260b7dbcc122e2aaffee7a6af9a
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-12 04:00:24 +0800

    Update LoadingState.hx
    Update loading box and text lol

--- source/LoadingState.hx
+++ source/LoadingState.hx
@@ -27,18 +27,22 @@ class LoadingState extends FlxState
 			FlxG.mouse.visible = true;
 		}
 
-		werningTxt = new FlxText(0, 0, FlxG.width, "", 30);
-		werningTxt.text = "WARNING\n" + "This game contains sexual and mature content.\n" + "Play it in a dark room all alone, and without your parents around you.\n" + "Enjoy the game, cupcake~..";
-		werningTxt.alignment = CENTER;
-		werningTxt.font = "assets/fonts/CascadiaCodePL-Regular.ttf";
+		werningTxt = new FlxText(0, 0, FlxG.width, "", 25);
+		werningTxt.setFormat(AssetPaths.CascadiaCodePL_Regular__ttf, 25, FlxColor.WHITE, CENTER);
+		werningTxt.text = "WARNING\n\n" + "This game contains sexual and mature content.\n" + "Play it in a dark room all alone, and without your parents around you.\n" + "Enjoy the game, cupcake~..";
 		werningTxt.screenCenter();
 		werningTxt.alpha = 0;
 		add(werningTxt);
 
-		loadCube = new FlxSprite().makeGraphic(100, 100, FlxColor.WHITE);
-		loadCube.screenCenter(X);
-		loadCube.y = werningTxt.y + 235;
+		loadCube = new FlxSprite().makeGraphic(50, 50, FlxColor.WHITE);
+		loadCube.x = 150 * 7.8;
+		loadCube.y = werningTxt.y + 325;
 		add(loadCube);
+
+		var secondLoadCube = new FlxSprite().makeGraphic(40, 40, FlxColor.BLACK);
+		secondLoadCube.x = loadCube.x + 5;
+		secondLoadCube.y = loadCube.y + 5;
+		add(secondLoadCube);
 
 		super.create();
 
@@ -46,15 +50,14 @@ class LoadingState extends FlxState
 		FlxG.camera.zoom = 0.85;
 
 		FlxTween.tween(FlxG.camera, {alpha: 1, zoom: 1}, 1.7, {ease: FlxEase.quartInOut});
-		FlxTween.tween(loadCube, {angle: 360}, 1.7, {ease: FlxEase.cubeInOut, type: FlxTweenType.LOOPING, loopDelay: 0.5});
+		FlxTween.tween(loadCube, {angle: 360}, 1.7, {ease: FlxEase.cubeInOut, type: FlxTweenType.LOOPING, loopDelay: 0.3});
+		FlxTween.tween(secondLoadCube, {angle: 360}, 1.7, {ease: FlxEase.cubeInOut, type: FlxTweenType.LOOPING, loopDelay: 0.3, startDelay: 0.05});
 		FlxTween.tween(werningTxt, {alpha: 1}, 1.7, {ease: FlxEase.cubeInOut});
 
 		new FlxTimer().start(11.5, function(tmr:FlxTimer) {
 			goingToState = true;
 			FlxTween.tween(FlxG.camera, {alpha: 0, zoom: 0.85}, 1.7, {ease: FlxEase.quartInOut});
-			FlxTween.tween(werningTxt, {alpha: 0}, 1.7, {ease: FlxEase.cubeInOut});
 		});
-
 	}
 
 	override public function update(elapsed:Float) 
