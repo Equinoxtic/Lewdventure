b0dc119d04fe00c1dba6b2fd052fb4fa50bc2878
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-03-11 17:11:04 +0800

    add level list :sunglasses:

--- source/LevelSelectionState.hx
+++ source/LevelSelectionState.hx
@@ -3,8 +3,45 @@ package;
 import flixel.FlxG;
 import flixel.FlxState;
 import flixel.FlxSprite;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
+import flixel.group.FlxGroup.FlxTypedGroup;
+
+using StringTools;
 
 class LevelSelectionState extends FlxState
 {
-	// eee.
+	var lvlList:FlxTypedGroup<FlxSprite>;
+	var lvlShit:Array<String> = [
+		"demo"
+	];
+	
+	override public function create()
+	{
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		bg.screenCenter();
+		add(bg);
+
+		lvlList = new FlxTypedGroup<FlxSprite>();
+		add(lvlList);
+
+		for (i in 0...lvlShit.length)
+		{
+			var lvlThumbnails:FlxSprite = new FlxSprite().loadGraphic("assets/images/levels/level_" + lvlShit[i] + ".png");
+			lvlThumbnails.setGraphicSize(Std.int(lvlThumbnails.width * 0.5));
+			lvlThumbnails.antialiasing = true;
+			lvlThumbnails.ID = i;
+			lvlList.add(lvlThumbnails);
+			lvlThumbnails.updateHitbox();
+		}
+
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
 }
